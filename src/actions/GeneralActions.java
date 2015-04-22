2bf11df3aea4554d00842dd515e0d93348036151
aleksandr-ua <aleksandr.pilipenko.ua@gmail.com>
2015-04-22 15:04:59 +0300

    JenkinsTest

--- src/actions/GeneralActions.java
+++ src/actions/GeneralActions.java
@@ -21,6 +21,7 @@ public class GeneralActions {
     }
 
     public void login(String name, String password) {
+        Reporter.log("Start");
         Reporter.log("Login");
         loginPage.openMainPage();
         loginPage.typeLoginName(name);
